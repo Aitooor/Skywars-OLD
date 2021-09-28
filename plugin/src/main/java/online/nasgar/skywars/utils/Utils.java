5053c2330b9cfff9fb42de8677c59d54edf6c30c
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-09-28 18:16:25 -0500

    Created Utils class

---
+++ plugin/src/main/java/online/nasgar/skywars/utils/Utils.java
@@ -0,0 +1,10 @@
+package online.nasgar.skywars.utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+
+    public static String ct(String source){
+        return ChatColor.translateAlternateColorCodes('&', source);
+    }
+}
