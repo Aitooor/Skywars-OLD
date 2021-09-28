89f4de9da6791e9e112b7ba2b090f9507808743e
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-09-28 18:11:57 -0500

    Created Cache file

---
+++ api/src/main/java/online/nasgar/skywars/api/cache/Cache.java
@@ -0,0 +1,10 @@
+package online.nasgar.skywars.api.cache;
+
+import online.nasgar.skywars.api.serializer.Serializable;
+
+/**
+ *
+ * @param <O>
+ */
+public interface Cache<O extends Serializable> {
+}
