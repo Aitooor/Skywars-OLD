82d907111a6138d058dcbae2b6ccbf8501b08f27
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-09-28 18:13:06 -0500

    Created GameState enum

---
+++ api/src/main/java/online/nasgar/skywars/api/game/state/GameState.java
@@ -0,0 +1,10 @@
+package online.nasgar.skywars.api.game.state;
+
+import online.nasgar.skywars.api.game.Game;
+
+/**
+ * Represents the {@link Game}'s state
+ */
+public enum GameState {
+    WAITING, STARTING, PLAYING, ENDING
+}
