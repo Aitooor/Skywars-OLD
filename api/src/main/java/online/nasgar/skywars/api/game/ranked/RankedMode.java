8d7ee776bd80770ebe943de45f961c0eb038a61a
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-09-28 18:15:14 -0500

    Added comment to the class

--- api/src/main/java/online/nasgar/skywars/api/game/ranked/RankedMode.java
+++ api/src/main/java/online/nasgar/skywars/api/game/ranked/RankedMode.java
@@ -1,5 +1,10 @@
 package online.nasgar.skywars.api.game.ranked;
 
+import online.nasgar.skywars.api.game.Game;
+
+/**
+ * Represents the {@link Game}'s ranked mode
+ */
 public enum RankedMode {
     SOLO, DOUBLES, SQUAD
 }
