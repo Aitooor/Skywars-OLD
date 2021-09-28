83455fd3dfc2997f16d96a1fd545bba028274bb1
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-09-28 18:12:30 -0500

    Created Game class

---
+++ api/src/main/java/online/nasgar/skywars/api/game/Game.java
@@ -0,0 +1,26 @@
+package online.nasgar.skywars.api.game;
+
+import online.nasgar.skywars.api.game.state.GameState;
+import online.nasgar.skywars.api.game.type.GameType;
+import online.nasgar.skywars.api.serializer.Serializable;
+import online.nasgar.skywars.api.user.User;
+
+import java.util.Set;
+
+/**
+ * Represents the model of a arena
+ */
+public interface Game extends Serializable {
+
+    Set<User> getPlayers();
+
+    Set<User> getAlive();
+
+    Set<User> getSpectators();
+
+    GameState getState();
+
+    GameType getType();
+
+    Integer getTeamSize();
+}
