59c29edda00d14ab54aea8bff50cf3bcb2b4397a
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-09-28 18:15:28 -0500

    Created GameType class

---
+++ api/src/main/java/online/nasgar/skywars/api/game/type/GameType.java
@@ -0,0 +1,10 @@
+package online.nasgar.skywars.api.game.type;
+
+import online.nasgar.skywars.api.game.Game;
+
+/**
+ * Represents the {@link Game}'s type
+ */
+public enum GameType {
+    SOLO, TEAM, RANKED;
+}
