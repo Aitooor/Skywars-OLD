2226c01aaf9af38682ff2f46541179ca661ee200
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-10-31 17:54:11 -0500

    Removed MySQLRepository class

--- plugin/src/main/java/online/nasgar/skywars/database/MySQLRepository.java
+++
@@ -1,9 +0,0 @@
-package online.nasgar.skywars.database;
-
-import online.nasgar.skywars.api.repository.Repository;
-import online.nasgar.skywars.api.serializer.Serializable;
-
-public class MySQLRepository<O extends Serializable> implements Repository<O> {
-
-
-}
