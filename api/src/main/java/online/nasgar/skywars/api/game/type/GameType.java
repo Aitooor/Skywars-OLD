f00942b50eac7a4246468cecf1950beab0f88e0b
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-10-31 17:32:59 -0500

    Changed GameType class to interface

--- api/src/main/java/online/nasgar/skywars/api/game/type/GameType.java
+++ api/src/main/java/online/nasgar/skywars/api/game/type/GameType.java
@@ -5,6 +5,39 @@ import online.nasgar.skywars.api.game.Game;
 /**
  * Represents the {@link Game}'s type.
  */
-public enum GameType {
-    SOLO, TEAM, RANKED;
+public interface GameType {
+
+    /**
+     * @see Game
+     *
+     * @return The {@link Game}'s team size.
+     */
+    Integer getSize();
+
+    /**
+     * @see #getSize()
+     *
+     * @param newSize The new team size of the
+     * {@link Game}
+     */
+    void setSize(Integer newSize);
+
+    /**
+     * @see Types
+     *
+     * @return The {@link Game}'s type.
+     */
+    Types getType();
+
+    /**
+     * @see #getType()
+     *
+     * @param newType The new type of the
+     * {@link Game}
+     */
+    void setType(Types newType);
+
+    enum Types {
+        UNRANKED, RANKED
+    }
 }
