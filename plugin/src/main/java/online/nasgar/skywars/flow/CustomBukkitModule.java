43304ef7204e2c95914fe432469ccd78259799cc
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-10-31 21:55:42 -0500

    Configure method fixed

--- plugin/src/main/java/online/nasgar/skywars/flow/CustomBukkitModule.java
+++ plugin/src/main/java/online/nasgar/skywars/flow/CustomBukkitModule.java
@@ -1,7 +1,14 @@
 package online.nasgar.skywars.flow;
 
 import me.fixeddev.commandflow.annotated.part.AbstractModule;
+import me.fixeddev.commandflow.annotated.part.Key;
+import me.fixeddev.commandflow.bukkit.annotation.Sender;
 import me.yushust.inject.Injector;
+import online.nasgar.skywars.api.game.Game;
+import online.nasgar.skywars.api.user.User;
+import online.nasgar.skywars.flow.factory.GamePartFactory;
+import online.nasgar.skywars.flow.factory.UserPartFactory;
+import online.nasgar.skywars.flow.factory.UserSenderPartFactory;
 
 public class CustomBukkitModule extends AbstractModule {
 
@@ -13,6 +20,8 @@ public class CustomBukkitModule extends AbstractModule {
 
     @Override
     public void configure() {
-
+        bindFactory(Game.class, new GamePartFactory(injector));
+        bindFactory(User.class, new UserPartFactory(injector));
+        bindFactory(new Key(User.class, Sender.class), new UserSenderPartFactory(injector));
     }
 }
