1b968a820f8d849e85c24bb618e243408fd45c4f
SoyMicrah <80792855+SoyMicrah@users.noreply.github.com>
2021-10-31 22:49:07 -0500

    Fixed CommandLoader

--- plugin/src/main/java/online/nasgar/skywars/loader/CommandLoader.java
+++ plugin/src/main/java/online/nasgar/skywars/loader/CommandLoader.java
@@ -1,16 +1,27 @@
 package online.nasgar.skywars.loader;
 
+import me.fixeddev.commandflow.CommandManager;
 import me.fixeddev.commandflow.annotated.AnnotatedCommandTreeBuilder;
+import me.fixeddev.commandflow.annotated.AnnotatedCommandTreeBuilderImpl;
+import me.fixeddev.commandflow.annotated.CommandClass;
+import me.fixeddev.commandflow.annotated.SubCommandInstanceCreator;
+import me.fixeddev.commandflow.annotated.builder.AnnotatedCommandBuilder;
 import me.fixeddev.commandflow.annotated.part.PartInjector;
 import me.fixeddev.commandflow.annotated.part.SimplePartInjector;
 import me.fixeddev.commandflow.annotated.part.defaults.DefaultsModule;
+import me.fixeddev.commandflow.bukkit.BukkitCommandManager;
 import me.fixeddev.commandflow.bukkit.factory.BukkitModule;
+import me.fixeddev.commandflow.translator.DefaultTranslator;
 import me.yushust.inject.InjectAll;
 import me.yushust.inject.Injector;
 import online.nasgar.skywars.SkyWars;
 import online.nasgar.skywars.api.loader.Loader;
+import online.nasgar.skywars.commands.SWArenaCommand;
 import online.nasgar.skywars.commands.SWCommand;
+import online.nasgar.skywars.flow.CustomAuthorizer;
 import online.nasgar.skywars.flow.CustomBukkitModule;
+import online.nasgar.skywars.flow.CustomTranslationProvider;
+import online.nasgar.skywars.flow.CustomUsageBuilder;
 
 import javax.inject.Inject;
 
@@ -18,8 +29,12 @@ import javax.inject.Inject;
 public class CommandLoader implements Loader {
 
     private SkyWars skyWars;
-    private Injector injector;
     private SWCommand swCommand;
+    private SWArenaCommand swArenaCommand;
+    private CustomTranslationProvider customTranslationProvider;
+    private CustomAuthorizer customAuthorizer;
+    private CustomUsageBuilder customUsageBuilder;
+    private Injector injector;
 
     @Override
     public void load() {
@@ -28,7 +43,36 @@ public class CommandLoader implements Loader {
         partInjector.install(new BukkitModule());
         partInjector.install(new CustomBukkitModule(injector));
 
-        AnnotatedCommandTreeBuilder annotatedCommandTreeBuilder = AnnotatedCommandTreeBuilder.create(partInjector);
+        AnnotatedCommandBuilder annotatedCommandBuilder = AnnotatedCommandBuilder.create(partInjector);
+        SubCommandInstanceCreator instanceCreator = (clazz, parent) -> injector.getInstance(clazz);
 
+        AnnotatedCommandTreeBuilder annotatedCommandTreeBuilder =
+                new AnnotatedCommandTreeBuilderImpl(
+                        annotatedCommandBuilder,
+                        instanceCreator
+                );
+
+        CommandManager commandManager = new BukkitCommandManager(skyWars.getName());
+        commandManager.setTranslator(new DefaultTranslator(customTranslationProvider));
+        commandManager.setAuthorizer(customAuthorizer);
+        commandManager.setUsageBuilder(customUsageBuilder);
+
+        registerCommands(
+                annotatedCommandTreeBuilder,
+                commandManager,
+                swCommand,
+                swArenaCommand
+        );
+    }
+
+    @Override
+    public void unload() {
+
+    }
+
+    private void registerCommands(AnnotatedCommandTreeBuilder annotatedCommandTreeBuilder, CommandManager commandManager, CommandClass... commandClasses) {
+        for (CommandClass commandClass : commandClasses) {
+            commandManager.registerCommands(annotatedCommandTreeBuilder.fromClass(commandClass));
+        }
     }
 }
